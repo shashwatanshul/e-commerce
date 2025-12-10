6b8ad9195ad5720959fc89d9858906ebc53659d6
Shashwat Singh <shashwatanshul@gmail.com>
2025-12-10 17:50:13 +0530

    /removed

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://e-commerce-frontend-two-delta.vercel.app/",
+    origin: "https://e-commerce-frontend-two-delta.vercel.app",
     credentials: true,
   })
 );
